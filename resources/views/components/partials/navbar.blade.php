8efa94d9d81b6659b18db5589a5dd272db1148ab
ikhwannaufal <ikhwannaufal.work@gmail.com>
2024-03-20 15:08:47 +0800

    Update navbar

--- resources/views/components/partials/navbar.blade.php
+++ resources/views/components/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow sticky-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">MoneyMatch</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -10,7 +10,7 @@
             <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="{{url('/categories')}}">Add</a>
           </li>
           
         </ul>
